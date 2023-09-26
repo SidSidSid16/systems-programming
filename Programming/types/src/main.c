508cb6b91755fe50c43f86073b38df6fe542da43
Sidharth Shanmugam <ss2985@its.york.ac.uk>
2023-09-26 10:08:19 +0100

    lab 2 - types

--- Programming/types/src/main.c
+++ Programming/types/src/main.c
@@ -7,6 +7,15 @@ int main(void) {
 	configUSART2(38400);
 	
 	printf("char: %d\r\n", sizeof(char));
+	
+	int x = 42000;
+	short y = 42;
+	y = (short) x;
+	
+	printf("x = %d, y = %hd\n", x,y);
+	
+	unsigned int mask = (0xBAUL) << 24;
+	printf("mask: 0x%08X\r\n", mask);
 
 	while(1);
 }
