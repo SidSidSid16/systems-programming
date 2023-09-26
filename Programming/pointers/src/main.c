7bdbd6cb65c85fe489059d9d35b11051f49e2e54
Sidharth Shanmugam <ss2985@its.york.ac.uk>
2023-09-26 10:50:26 +0100

    lab 2 - pointers, todo

--- Programming/pointers/src/main.c
+++ Programming/pointers/src/main.c
@@ -1,12 +1,19 @@
 #include "Utils/utils.h"
 #include <stdio.h>
 
+
+static void square(long long *i) {
+	
+}
+
 int main(void) {
 	
 	configClock();
 	configUSART2(38400);
 	
 	int i = 3;
+	
+	printf("i = %p, i+1 = %p\n", (void *) &i, (void *) ((&i)+1));
 	
 	while(1);
 
