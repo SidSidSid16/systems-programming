20622083e3f0f4961f3cd7c48c1cde289fc8144b
Sidharth Shanmugam <ss2985@its.york.ac.uk>
2023-09-28 16:35:10 +0100

    lab 2 - arrays

--- Programming/arrays/src/main.c
+++ Programming/arrays/src/main.c
@@ -1,6 +1,11 @@
 #include "Utils/utils.h"
 #include <stdio.h>
 
+static void processArray(int *array) {
+	*(array + 2) = 10;
+}
+
+
 int main(void) {
 	
 	configClock();
@@ -9,8 +14,23 @@ int main(void) {
 	int numbers[] = {1,3,5,7,9};
 
 	numbers[0] = 11;
-	numbers[3] = 42;
+	*(numbers + 3) = 42;
+	
+	int *pointer = &(numbers[0]);
 
+	for (int i = 0; i < 5; ++i) {
+		printf("numbers[%d] = %d\n", i, numbers[i]);
+	}
+	
+	for (int i = 0; i < 5; ++i) {
+		printf("pointer[%d] = %d\n", i, pointer[i]);
+	}
+	
+	printf("sizeof array: %d\n", sizeof(numbers));
+	printf("sizeof pointer: %d\n", sizeof(pointer));
+	
+	processArray(numbers);
+	
 	for (int i = 0; i < 5; ++i) {
 		printf("numbers[%d] = %d\n", i, numbers[i]);
 	}
