56e8990417fb102ddef66d2a9e097c357000d771
Sidharth Shanmugam <ss2985@its.york.ac.uk>
2023-10-02 10:59:25 +0100

    lab 2

--- Programming/wordcount/src/main.c
+++ Programming/wordcount/src/main.c
@@ -2,12 +2,41 @@
 #include <stdio.h>
 #include <string.h>
 
-static int wordcount(char * string) {
-	int words = 0;
+static int isAlpha(char *string) {
+	return (*string >= '0' && *string <= '9') || (*string >= 'a' && *string <= 'z') || (*string >= 'A' && *string <= 'Z');
+}
 
+static int wordcountNext(char * string) {
+	int words = 0;
+	while (*string) {
+		if (isAlpha(string)) {
+			if (!isAlpha(string+1)) {
+				words++;
+			}
+		}
+		string++;
+	}
+	return words;	
+}
+
+static int wordcountNextInv(char * string) {
+	int words = 0;
+	while (*string) {
+		if (!isAlpha(string)) {
+			if (isAlpha(string+1)) {
+				words++;
+			}
+		}
+		string++;
+	}
+	return words;	
+}
+
+static int wordcountFlag(char * string) {
+	int words = 0;
 	int inWord = 0;
 	while (*string) {
-		if ((*string >= '0' && *string <= '9') || (*string >= 'a' && *string <= 'z') || (*string >= 'A' && *string <= 'Z')) {
+		if (isAlpha(string)) {
 			if (!inWord) {
 				inWord = 1;
 				words++;
@@ -17,8 +46,6 @@ static int wordcount(char * string) {
 		}
 		string++;
 	}
-	
-
 	return words;
 }
 
@@ -28,9 +55,11 @@ int main(void) {
 	configUSART2(38400);
 	
 	// This text contains 11 words.
-	char * text = "This is a  word count test.\nHere is the second line !";
+	char * text = ", This is a  word count test.\nHere is the second line\nAndy Andy Andrew Pomfret!";
 	printf("The text is:\n%s\n", text);
-	printf("It contains %d words.\n", wordcount(text));
+	printf("It contains %d words.\n", wordcountNext(text));
+	printf("It contains %d words.\n", wordcountNextInv(text));
+	printf("It contains %d words.\n", wordcountFlag(text));
 
 	while(1);
 }
