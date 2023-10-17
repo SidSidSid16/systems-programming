671d87d33fe2a5add327796f378a76339f91049a
Sidharth Shanmugam <ss2985@its.york.ac.uk>
2023-10-17 10:23:54 +0100

    lab 5, section 5.4 (not 5.4.1) done

--- Allocation/mempool/src/mempool.c
+++ Allocation/mempool/src/mempool.c
@@ -3,8 +3,19 @@
 void *pool_allocate(mempool_t *pool) {
 	// Return the head of the list of blocks
 	// Update the head pointer
+	mempool_item_t * headItem = pool->head;
+	if (headItem) {
+		pool->head = headItem->next;
+		return headItem;
+	}
+	return 0;
 }
 
 void pool_deallocate(mempool_t *pool, void *block) {
 	// Add the new item to the head of the list
+	// Point the 'next' parameter of the block to point to the current head of the pool
+	mempool_item_t *item = block;
+	item->next = pool->head;
+	// Overwrite the head of the pool to point to the new block being stored.
+	pool->head = item;
 }
