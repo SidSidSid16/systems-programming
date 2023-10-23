eb5eaea6358571cbcf8ab49174526c662654dc02
Sidharth Shanmugam <ss2985@its.york.ac.uk>
2023-10-23 11:11:09 +0100

    lab 6, fibonacci needs testing

--- Assembly/fibonacci/asm/main.s
+++ Assembly/fibonacci/asm/main.s
@@ -7,7 +7,22 @@ array_end
 	EXPORT asm_main
 asm_main
 	; Your code goes here
-
+	LDR r0, =array
+	LDR r1, =array_end
+	MOV r2, #1
+	MOV r3, #1
+	
+fibonacci
+	CMP r0, r1			; compare start and end address
+	BHS end_fibonacci	; end loop if r0 >= r1
+	
+	STR r2, [r0] #4		; store memory item into r2, increment addr by 4 bytes
+	MOV r4, r2
+	ADD r2, r3
+	MOV r3, r4
+	
+end_fibonacci
+	
 	B .			; Loop forever
 
 	ALIGN
