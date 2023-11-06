131db1a8e11d6ac3a4575aff5cbf0de40116378a
Sidharth Shanmugam <ss2985@its.york.ac.uk>
2023-11-06 09:37:42 +0000

    lab 7, 7.2 done

---
+++ Interwork/export/Objects/main.d
@@ -0,0 +1,3 @@
+./objects/main.o: src\main.c \
+  c:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdio.h \
+  c:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h inc\Utils\utils.h
--- Interwork/export/asm/asm.s
+++ Interwork/export/asm/asm.s
@@ -1,5 +1,12 @@
 	AREA mainarea,CODE
+	EXPORT calculate
 
-	; Your instructions go here...
+calculate
+	STMFD sp!, {r4, lr}
+	
+	ADD r4, r0, r0, LSL #2		; r4 = r0 + 4*r0 = 5*r0
+	ADD r0, r4, r1				; r0 = r4 + r1 = 5*r0 + y
+	
+	LDMFD sp!, {r4, pc}
 
 	END
