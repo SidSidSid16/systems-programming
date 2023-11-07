4a301902f267526f9b522620d4fef037accaae86
Sidharth Shanmugam <ss2985@its.york.ac.uk>
2023-11-07 09:24:38 +0000

    lab 7, 7.4.2 completed

--- Interwork/structs/src/main.c
+++ Interwork/structs/src/main.c
@@ -18,8 +18,11 @@ typedef struct {
 void process(structure_t * s);
 
 static void printInteger(void * arg) {
-	int32_t * arg_int = arg;
-	printf("%" PRId32 "\r\n", *arg_int);
+	printf("print integer: %" PRId32 "\r\n", *(int32_t *)arg);
+}
+
+static void printDouble(void * arg) {
+	printf("print double: %f \r\n", *(double *)arg);
 }
 
 int main(void) {
@@ -39,6 +42,22 @@ int main(void) {
 	
 	printf("x = %" PRId32 "\r\n", test_struct.x);
 	printf("y = %" PRId32 "\r\n", test_struct.y);
+	
+	
+	
+	double another_test_ptr = 11;
+	
+	structure_t another_test_struct = {
+		.x = 12,
+		.y = 21,
+		.ptr = &another_test_ptr,
+		.callback = printDouble
+	};
+	
+	process(&another_test_struct);
+	
+	printf("x = %" PRId32 "\r\n", another_test_struct.x);
+	printf("y = %" PRId32 "\r\n\n\n\n", another_test_struct.y);
 
 	while(1);
 }
