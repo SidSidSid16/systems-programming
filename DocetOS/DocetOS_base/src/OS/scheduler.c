f4c2a85cc5f3346a4cfd5c39bcbfb1de7f7c3fb6
Sidharth Shanmugam <ss2985@its.york.ac.uk>
2023-11-19 17:32:20 +0000

    lab 9, completed 9.3

--- DocetOS/DocetOS_base/src/OS/scheduler.c
+++ DocetOS/DocetOS_base/src/OS/scheduler.c
@@ -36,7 +36,14 @@ static void list_add(_OS_tasklist_t *list, OS_TCB_t *task) {
 }
 
 static void list_remove(_OS_tasklist_t *list, OS_TCB_t *task) {
-	/* Not implemented yet */
+	if (task->next == task) {
+		list->head = 0;
+		return;
+	} else if (list->head == task) {
+		list->head = task->next;
+	}
+	task->next->prev = task->prev;
+	task->prev->next = task->next;
 }
 
 /* Round-robin scheduler */
