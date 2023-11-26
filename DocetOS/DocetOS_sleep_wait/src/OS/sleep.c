c193c14471bb9dab561ff9a7316ffdfa82b8ebfe
Sidharth Shanmugam <ss2985@its.york.ac.uk>
2023-11-26 18:32:59 +0000

    lab 10, 10.2 complete

---
+++ DocetOS/DocetOS_sleep_wait/src/OS/sleep.c
@@ -0,0 +1,17 @@
+#define OS_INTERNAL
+
+#include "OS/os.h"
+#include "OS/sleep.h"
+
+void OS_sleep(uint32_t sleepDuration) {
+	// The running task's TCB is retrieved and stored
+	OS_TCB_t * currentTCB = OS_currentTCB();
+	// wakeTime time is calculated by adding sleepDuration to the elapsed OS ticks
+	uint32_t wakeTime = OS_elapsedTicks() + sleepDuration;
+	// wakeTime can be stored in TCB in the data field
+	currentTCB->data = &wakeTime;
+	// Set the TCB state to sleeping
+	currentTCB->state |= TASK_STATE_SLEEP;
+	// Initiate task switch by calling yield
+	OS_yield();
+}
